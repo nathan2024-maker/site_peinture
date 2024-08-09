e1edf3376e28eade708b4993242d91f259c555db
SODO KANGNI KOMLAN <lyffo@yahoo.fr>
2024-08-09 23:33:30 +0000

    creationentite categorie

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,16 @@ class Categorie
      * @ORM\Column(type="string", length=255)
      */
     private $slug;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Peinture::class, mappedBy="categorie")
+     */
+    private $peintures;
+
+    public function __construct()
+    {
+        $this->peintures = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +81,33 @@ class Categorie
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Peinture>
+     */
+    public function getPeintures(): Collection
+    {
+        return $this->peintures;
+    }
+
+    public function addPeinture(Peinture $peinture): self
+    {
+        if (!$this->peintures->contains($peinture)) {
+            $this->peintures[] = $peinture;
+            $peinture->addCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removePeinture(Peinture $peinture): self
+    {
+        if ($this->peintures->removeElement($peinture)) {
+            $peinture->removeCategorie($this);
+        }
 
         return $this;
     }
